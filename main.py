d04c27ede5f947b1ce211a8de1fc5f65eec65575
Sathabhron <thahi5_vi@hotmail.com>
2023-01-03 20:36:06 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import psycopg2
+class Postgres:
+
+    # class attribute
+    database_address = ''
+    database_name = ''
+    database_username = ''
+    database_password = ''
+    database_port = ''
+
+    database_connection = None
+    database_query_result = None
+
+    _reconnectTries = 5
+    _reconnectIdle = 2
+
+    # instance attribute
+    def __init__(self, address, dbname, username, password, port):
+        #self.name = name
+        self.database_address = address
+        self.database_name = dbname
+        self.database_username = username
+        self.database_password = password
+        self.database_port = port
+
+    def connect(self):
+        conn = psycopg2.connect(
+            dbname = self.database_name,
+            user = self.database_username,
+            password = self.database_password,
+            host = self.database_address,
+            port = self.database_port
+        )
+        conn.autocommit = True
+        try:
+            self.database_connection = conn.cursor()
+        except psycopg2.Error as e:
+            print(e)
+
+    def reset(self):
+        if self.database_connection:
+            self.database_connection.close()
+        self.database_connection = None
+
+    def execute(self, data):
+        sqlState = data
+        try:
+            self.database_connection.execute(sqlState)
+        except psycopg2.OperationalError as error:
+            self.reset()
+            self.connect()
+            self.database_connection.execute(sqlState)
+        except (Exception, psycopg2.Error) as error:
+            raise error
